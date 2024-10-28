7c3c546c2b8ec67f58b1997807625c374ffc454a
Matheo Ducrot <matheo.ducrot@epita.fr>
2024-10-28 08:56:06 -0400

    initial

---
+++ MainWidget/MainWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  MainWidgetBundle.swift
+//  MainWidget
+//
+//  Created by Mathéo Ducrot on 28/10/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MainWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MainWidget()
+        MainWidgetControl()
+    }
+}
---
+++ SolidBank/SolidBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidBankApp.swift
+//  SolidBank
+//
+//  Created by Mathéo Ducrot on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SolidBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
