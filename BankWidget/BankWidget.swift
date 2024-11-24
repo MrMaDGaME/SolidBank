6dd1b2c668843bd7be3523b979fc2b9de07ff223
Aubin Rochefort <135309486+Aubin35@users.noreply.github.com>
2024-11-24 17:16:57 -0500

    widget sizes

--- BankWidget/BankWidget.swift
+++ BankWidget/BankWidget.swift
@@ -10,28 +10,36 @@ import SwiftUI
 
 struct BankWidgetEntry: TimelineEntry {
     let date: Date
-    let account: Account?
+    let accounts: [Account]
 }
 
 struct BankWidgetProvider: TimelineProvider {
     func placeholder(in context: Context) -> BankWidgetEntry {
-        BankWidgetEntry(date: Date(), account: Account(name: "Compte Exemple", balance: 100.0))
+        BankWidgetEntry(date: Date(),
+                        accounts: [Account(name: "Compte Exemple", balance: 100.0),
+                                   Account(name: "Compte Exemple", balance: 152.68)])
     }
 
     func getSnapshot(in context: Context, completion: @escaping (BankWidgetEntry) -> ()) {
-        let entry = BankWidgetEntry(date: Date(), account: Account(name: "Compte Exemple", balance: 100.0))
+        let entry = BankWidgetEntry(date: Date(),
+                                    accounts: [Account(name: "Compte Exemple", balance: 100.0),
+                                               Account(name: "Compte Exemple", balance: 152.68)])
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<BankWidgetEntry>) -> ()) {
         let accountsData = UserDefaults(suiteName: "group.com.solidbank")?.data(forKey: "accounts")
-        var account: Account? = nil
+        var accounts: [Account] = [ // Valeurs par défaut en cas d'erreur
+            Account(name: "Compte Exemple", balance: 100.0),
+            Account(name: "Compte Exemple", balance: 152.68),
+            Account(name: "Compte Exemple", balance: 100.0),
+            Account(name: "Compte Exemple", balance: 152.68)]
 
         if let data = accountsData, let decoded = try? JSONDecoder().decode([Account].self, from: data) {
-            account = decoded.first
+            accounts = Array(decoded.prefix(4))
         }
 
-        let entry = BankWidgetEntry(date: Date(), account: account)
+        let entry = BankWidgetEntry(date: Date(), accounts: accounts)
         let timeline = Timeline(entries: [entry], policy: .atEnd)
         completion(timeline)
     }
@@ -39,20 +47,57 @@ struct BankWidgetProvider: TimelineProvider {
 
 struct BankWidgetEntryView: View {
     var entry: BankWidgetProvider.Entry
-
+    @Environment(\.widgetFamily) var widgetFamily
+    
     var body: some View {
-        VStack {
-            if let account = entry.account {
-                Text(account.name)
-                    .font(.headline)
-                Text("Solde: $\(account.balance, specifier: "%.2f")")
-                    .font(.subheadline)
-            } else {
-                Text("Aucun compte sélectionné")
-                    .font(.headline)
+        switch widgetFamily{
+        case .systemSmall:
+            VStack {
+                if let accounts = entry.accounts.first {
+                    Text(accounts.name)
+                        .font(.headline)
+                    Text("Solde: $\(accounts.balance, specifier: "%.2f")")
+                        .font(.subheadline)
+                } else {
+                    Text("Aucun compte sélectionné")
+                        .font(.headline)
+                }
             }
+            .padding()
+            
+        case .systemMedium:
+            HStack {
+                ForEach(entry.accounts.prefix(2)) { account in
+                    VStack {
+                        Text(account.name)
+                            .font(.headline)
+                            .multilineTextAlignment(.center)
+                        Text("Solde: $\(account.balance, specifier: "%.2f")")
+                            .font(.subheadline)
+                            .multilineTextAlignment(.center)
+                    }
+                    .frame(maxWidth: .infinity) // Répartition égale de l'espace
+                }
+            }
+            .padding()
+            
+        case .systemLarge:
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
+                        ForEach(entry.accounts.prefix(4)) { account in
+                            VStack {
+                                Text(account.name)
+                                    .font(.headline)
+                                Text("Solde: $\(account.balance, specifier: "%.2f")")
+                                    .font(.subheadline)
+                            }
+                            .frame(maxWidth: .infinity)
+                        }
+                    }
+                    .padding()
+            
+        default:
+            Text("Non supporté")
         }
-        .padding()
     }
 }
 
@@ -72,12 +117,13 @@ struct BankWidget: Widget {
         }
         .configurationDisplayName("Solde de Compte Bancaire")
         .description("Affiche le solde d'un compte bancaire.")
-        .supportedFamilies([.systemSmall])
+        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
     }
 }
 
 #Preview(as: .systemSmall) {
     BankWidget()
 } timeline: {
-    BankWidgetEntry(date: .now, account: .init(name: "Compte Exemple", balance: 100))
+    BankWidgetEntry(date: .now, accounts: [Account(name: "Compte Exemple", balance: 100),
+                                           Account(name: "Compte Exemple", balance: 152.68)])
 }
