14c8f96abe42ba1f72f997336504d5b8a0277b8c
paschyz <paschy2002@gmail.com>
2024-11-27 19:09:30 -0500

    new widget version

--- BankWidget/BankWidget.swift
+++ BankWidget/BankWidget.swift
@@ -7,39 +7,32 @@
 
 import WidgetKit
 import SwiftUI
-
+import AppIntents
 struct BankWidgetEntry: TimelineEntry {
     let date: Date
-    let accounts: [Account]
+    let account: Account?
 }
+
 
 struct BankWidgetProvider: TimelineProvider {
     func placeholder(in context: Context) -> BankWidgetEntry {
-        BankWidgetEntry(date: Date(),
-                        accounts: [Account(name: "Compte Exemple", balance: 100.0),
-                                   Account(name: "Compte Exemple", balance: 152.68)])
+        BankWidgetEntry(date: Date(), account: Account(name: "Compte Exemple", balance: 100.0))
     }
 
     func getSnapshot(in context: Context, completion: @escaping (BankWidgetEntry) -> ()) {
-        let entry = BankWidgetEntry(date: Date(),
-                                    accounts: [Account(name: "Compte Exemple", balance: 100.0),
-                                               Account(name: "Compte Exemple", balance: 152.68)])
+        let entry = BankWidgetEntry(date: Date(), account: Account(name: "Compte Exemple", balance: 100.0))
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<BankWidgetEntry>) -> ()) {
         let accountsData = UserDefaults(suiteName: "group.com.solidbank")?.data(forKey: "accounts")
-        var accounts: [Account] = [ // Valeurs par défaut en cas d'erreur
-            Account(name: "Compte Exemple", balance: 100.0),
-            Account(name: "Compte Exemple", balance: 152.68),
-            Account(name: "Compte Exemple", balance: 100.0),
-            Account(name: "Compte Exemple", balance: 152.68)]
+        var account: Account? = nil
 
         if let data = accountsData, let decoded = try? JSONDecoder().decode([Account].self, from: data) {
-            accounts = Array(decoded.prefix(4))
+            account = decoded.first
         }
 
-        let entry = BankWidgetEntry(date: Date(), accounts: accounts)
+        let entry = BankWidgetEntry(date: Date(), account: account)
         let timeline = Timeline(entries: [entry], policy: .atEnd)
         completion(timeline)
     }
@@ -47,56 +40,130 @@ struct BankWidgetProvider: TimelineProvider {
 
 struct BankWidgetEntryView: View {
     var entry: BankWidgetProvider.Entry
-    @Environment(\.widgetFamily) var widgetFamily
-    
+
     var body: some View {
-        switch widgetFamily{
-        case .systemSmall:
+        VStack {
+            if let account = entry.account {
+                Text(account.name)
+                    .font(.headline)
+                Text("Solde: $\(account.balance, specifier: "%.2f")")
+                    .font(.subheadline)
+            } else {
+                Text("Aucun compte sélectionné")
+                    .font(.headline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct WidgetColor: AppEntity {
+    var id: String
+    var account: Account
+    
+    static var typeDisplayRepresentation: TypeDisplayRepresentation = "Widget Account"
+    static var defaultQuery = WidgetColorQuery()
+    
+
+    var displayRepresentation: DisplayRepresentation {
+        DisplayRepresentation(
+            title: "\(account.name)",
+            subtitle: "Solde: \(String(format: "%.2f", account.balance)) $"
+        )
+    }
+    let accountsData = UserDefaults(suiteName: "group.com.solidbank")?.data(forKey: "accounts")
+    
+    static let allAccounts: [WidgetColor] = {
+        return WidgetColor.loadAccountsFromUserDefaults()
+    }()
+    
+    static func loadAccountsFromUserDefaults() -> [WidgetColor] {
+        guard let data = UserDefaults(suiteName: "group.com.solidbank")?.data(forKey: "accounts") else {
+            print("No data found in UserDefaults.")
+            return []
+        }
+        
+        do {
+            // Decode the data into Account objects
+            let accounts = try JSONDecoder().decode([Account].self, from: data)
+            
+            // Map each Account object to a WidgetColor instance
+            return accounts.map { WidgetColor(id: $0.id.uuidString, account: $0) }
+        } catch {
+            print("Failed to decode accounts: \(error)")
+            return []
+        }
+    }
+}
+
+
+struct WidgetColorQuery: EntityQuery {
+    func entities(for identifiers: [WidgetColor.ID]) async throws -> [WidgetColor] {
+        WidgetColor.allAccounts.filter{
+            identifiers.contains($0.id)
+        }
+    }
+    
+    func suggestedEntities() async throws -> [WidgetColor] {
+        WidgetColor.allAccounts
+    }
+    
+    func defaultResult() async -> WidgetColor? {
+        WidgetColor.allAccounts.first
+    }
+}
+
+struct SelectColorIntent: WidgetConfigurationIntent{
+    static var title:LocalizedStringResource = "Modifier compte"
+    static var description:IntentDescription = IntentDescription("Description")
+    
+    @Parameter(title: "Selected account")
+    var accentColor: WidgetColor?
+}
+
+struct ColoredTimeline: AppIntentTimelineProvider {
+    func timeline(for configuration: SelectColorIntent, in context: Context) async -> Timeline<ColoredEntry> {
+        let color = configuration.accentColor ?? WidgetColor.allAccounts.first!
+        return Timeline(entries: [ColoredEntry(date: .now, widgetColor: color)], policy: .never)
+    }
+    
+    func snapshot(for configuration: SelectColorIntent, in context: Context) async -> ColoredEntry {
+        let color = configuration.accentColor ?? WidgetColor.allAccounts.first!
+        return ColoredEntry(date: .now, widgetColor: color)
+    }
+    
+    func placeholder(in context: Context) -> ColoredEntry {
+        ColoredEntry(date: .now, widgetColor: WidgetColor.allAccounts.first!)
+    }
+}
+
+
+struct ColoredEntry: TimelineEntry{
+    let date: Date
+    let widgetColor: WidgetColor
+}
+
+struct ColoredWidgetView: View {
+    let entry: ColoredEntry
+
+    var body: some View {
+        ZStack {
             VStack {
-                if let accounts = entry.accounts.first {
-                    Text(accounts.name)
-                        .font(.headline)
-                    Text("Solde: $\(accounts.balance, specifier: "%.2f")")
-                        .font(.subheadline)
-                } else {
-                    Text("Aucun compte sélectionné")
-                        .font(.headline)
-                }
+                Text("Solde: \(String(format: "%.2f", entry.widgetColor.account.balance))$")
+                    .font(.headline)
+                    .foregroundColor(.black)
+                    .padding(.bottom, 4)
+                    
+
+                // Display account name
+                Text(entry.widgetColor.account.name)
+                    .font(.subheadline)
+                    .foregroundColor(.black)
             }
             .padding()
-            
-        case .systemMedium:
-            HStack {
-                ForEach(entry.accounts.prefix(2)) { account in
-                    VStack {
-                        Text(account.name)
-                            .font(.headline)
-                            .multilineTextAlignment(.center)
-                        Text("Solde: $\(account.balance, specifier: "%.2f")")
-                            .font(.subheadline)
-                            .multilineTextAlignment(.center)
-                    }
-                    .frame(maxWidth: .infinity) // Répartition égale de l'espace
-                }
-            }
-            .padding()
-            
-        case .systemLarge:
-                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
-                        ForEach(entry.accounts.prefix(4)) { account in
-                            VStack {
-                                Text(account.name)
-                                    .font(.headline)
-                                Text("Solde: $\(account.balance, specifier: "%.2f")")
-                                    .font(.subheadline)
-                            }
-                            .frame(maxWidth: .infinity)
-                        }
-                    }
-                    .padding()
-            
-        default:
-            Text("Non supporté")
+        }
+        .containerBackground(for: .widget) {
+            Color.clear
         }
     }
 }
@@ -105,25 +172,18 @@ struct BankWidget: Widget {
     let kind: String = "BankWidget"
 
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: BankWidgetProvider()) { entry in
-            if #available(iOS 17.0, *) {
-                BankWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-            } else {
-                BankWidgetEntryView(entry: entry)
-                    .padding()
-                    .background()
-            }
+        AppIntentConfiguration(kind: "ColoredWidget",intent: SelectColorIntent.self ,provider: ColoredTimeline() ){
+            entry in ColoredWidgetView(entry:entry)
         }
-        .configurationDisplayName("Solde de Compte Bancaire")
-        .description("Affiche le solde d'un compte bancaire.")
-        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
+        .configurationDisplayName("Modify Widget")
+        .description("Choose Account")
+        .supportedFamilies([.systemSmall])
+        .contentMarginsDisabled()
     }
 }
 
 #Preview(as: .systemSmall) {
     BankWidget()
 } timeline: {
-    BankWidgetEntry(date: .now, accounts: [Account(name: "Compte Exemple", balance: 100),
-                                           Account(name: "Compte Exemple", balance: 152.68)])
+    ColoredEntry(date: .now, widgetColor: WidgetColor.allAccounts.first!)
 }
