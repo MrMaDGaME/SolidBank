9353b3f08d320bd7b75c8d503fd513dff691dce1
paschyz <paschy2002@gmail.com>
2024-11-28 13:51:33 -0500

    cleaned up unused struct and variable name change in widget

--- BankWidget/BankWidget.swift
+++ BankWidget/BankWidget.swift
@@ -8,61 +8,13 @@
 import WidgetKit
 import SwiftUI
 import AppIntents
-struct BankWidgetEntry: TimelineEntry {
-    let date: Date
-    let account: Account?
-}
 
-
-struct BankWidgetProvider: TimelineProvider {
-    func placeholder(in context: Context) -> BankWidgetEntry {
-        BankWidgetEntry(date: Date(), account: Account(name: "Compte Exemple", balance: 100.0))
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (BankWidgetEntry) -> ()) {
-        let entry = BankWidgetEntry(date: Date(), account: Account(name: "Compte Exemple", balance: 100.0))
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<BankWidgetEntry>) -> ()) {
-        let accountsData = UserDefaults(suiteName: "group.com.solidbank")?.data(forKey: "accounts")
-        var account: Account? = nil
-
-        if let data = accountsData, let decoded = try? JSONDecoder().decode([Account].self, from: data) {
-            account = decoded.first
-        }
-
-        let entry = BankWidgetEntry(date: Date(), account: account)
-        let timeline = Timeline(entries: [entry], policy: .atEnd)
-        completion(timeline)
-    }
-}
-
-struct BankWidgetEntryView: View {
-    var entry: BankWidgetProvider.Entry
-
-    var body: some View {
-        VStack {
-            if let account = entry.account {
-                Text(account.name)
-                    .font(.headline)
-                Text("Solde: $\(account.balance, specifier: "%.2f")")
-                    .font(.subheadline)
-            } else {
-                Text("Aucun compte sélectionné")
-                    .font(.headline)
-            }
-        }
-        .padding()
-    }
-}
-
-struct WidgetColor: AppEntity {
+struct WidgetAccount: AppEntity {
     var id: String
     var account: Account
     
     static var typeDisplayRepresentation: TypeDisplayRepresentation = "Widget Account"
-    static var defaultQuery = WidgetColorQuery()
+    static var defaultQuery = WidgetAccountQuery()
     
 
     var displayRepresentation: DisplayRepresentation {
@@ -73,11 +25,11 @@ struct WidgetColor: AppEntity {
     }
     let accountsData = UserDefaults(suiteName: "group.com.solidbank")?.data(forKey: "accounts")
     
-    static let allAccounts: [WidgetColor] = {
-        return WidgetColor.loadAccountsFromUserDefaults()
+    static let allAccounts: [WidgetAccount] = {
+        return WidgetAccount.loadAccountsFromUserDefaults()
     }()
     
-    static func loadAccountsFromUserDefaults() -> [WidgetColor] {
+    static func loadAccountsFromUserDefaults() -> [WidgetAccount] {
         guard let data = UserDefaults(suiteName: "group.com.solidbank")?.data(forKey: "accounts") else {
             print("No data found in UserDefaults.")
             return []
@@ -87,8 +39,8 @@ struct WidgetColor: AppEntity {
             // Decode the data into Account objects
             let accounts = try JSONDecoder().decode([Account].self, from: data)
             
-            // Map each Account object to a WidgetColor instance
-            return accounts.map { WidgetColor(id: $0.id.uuidString, account: $0) }
+            // Map each Account object to a WidgetAccount instance
+            return accounts.map { WidgetAccount(id: $0.id.uuidString, account: $0) }
         } catch {
             print("Failed to decode accounts: \(error)")
             return []
@@ -97,66 +49,66 @@ struct WidgetColor: AppEntity {
 }
 
 
-struct WidgetColorQuery: EntityQuery {
-    func entities(for identifiers: [WidgetColor.ID]) async throws -> [WidgetColor] {
-        WidgetColor.allAccounts.filter{
+struct WidgetAccountQuery: EntityQuery {
+    func entities(for identifiers: [WidgetAccount.ID]) async throws -> [WidgetAccount] {
+        WidgetAccount.allAccounts.filter{
             identifiers.contains($0.id)
         }
     }
     
-    func suggestedEntities() async throws -> [WidgetColor] {
-        WidgetColor.allAccounts
+    func suggestedEntities() async throws -> [WidgetAccount] {
+        WidgetAccount.allAccounts
     }
     
-    func defaultResult() async -> WidgetColor? {
-        WidgetColor.allAccounts.first
+    func defaultResult() async -> WidgetAccount? {
+        WidgetAccount.allAccounts.first
     }
 }
 
-struct SelectColorIntent: WidgetConfigurationIntent{
-    static var title:LocalizedStringResource = "Modifier compte"
-    static var description:IntentDescription = IntentDescription("Description")
+struct SelectAccountIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Modifier compte"
+    static var description: IntentDescription = IntentDescription("Description")
     
     @Parameter(title: "Selected account")
-    var accentColor: WidgetColor?
+    var selectedAccount: WidgetAccount?
 }
 
-struct ColoredTimeline: AppIntentTimelineProvider {
-    func timeline(for configuration: SelectColorIntent, in context: Context) async -> Timeline<ColoredEntry> {
-        let color = configuration.accentColor ?? WidgetColor.allAccounts.first!
-        return Timeline(entries: [ColoredEntry(date: .now, widgetColor: color)], policy: .never)
+struct AccountTimeline: AppIntentTimelineProvider {
+    func timeline(for configuration: SelectAccountIntent, in context: Context) async -> Timeline<AccountEntry> {
+        let account = configuration.selectedAccount ?? WidgetAccount.allAccounts.first!
+        return Timeline(entries: [AccountEntry(date: .now, widgetAccount: account)], policy: .never)
     }
     
-    func snapshot(for configuration: SelectColorIntent, in context: Context) async -> ColoredEntry {
-        let color = configuration.accentColor ?? WidgetColor.allAccounts.first!
-        return ColoredEntry(date: .now, widgetColor: color)
+    func snapshot(for configuration: SelectAccountIntent, in context: Context) async -> AccountEntry {
+        let account = configuration.selectedAccount ?? WidgetAccount.allAccounts.first!
+        return AccountEntry(date: .now, widgetAccount: account)
     }
     
-    func placeholder(in context: Context) -> ColoredEntry {
-        ColoredEntry(date: .now, widgetColor: WidgetColor.allAccounts.first!)
+    func placeholder(in context: Context) -> AccountEntry {
+        AccountEntry(date: .now, widgetAccount: WidgetAccount.allAccounts.first!)
     }
 }
 
 
-struct ColoredEntry: TimelineEntry{
+struct AccountEntry: TimelineEntry {
     let date: Date
-    let widgetColor: WidgetColor
+    let widgetAccount: WidgetAccount
 }
 
-struct ColoredWidgetView: View {
-    let entry: ColoredEntry
+struct AccountWidgetView: View {
+    let entry: AccountEntry
 
     var body: some View {
         ZStack {
             VStack {
-                Text("Solde: \(String(format: "%.2f", entry.widgetColor.account.balance))$")
+                Text("Solde: \(String(format: "%.2f", entry.widgetAccount.account.balance))$")
                     .font(.headline)
                     .foregroundColor(.black)
                     .padding(.bottom, 4)
                     
 
                 // Display account name
-                Text(entry.widgetColor.account.name)
+                Text(entry.widgetAccount.account.name)
                     .font(.subheadline)
                     .foregroundColor(.black)
             }
@@ -172,8 +124,8 @@ struct BankWidget: Widget {
     let kind: String = "BankWidget"
 
     var body: some WidgetConfiguration {
-        AppIntentConfiguration(kind: "ColoredWidget",intent: SelectColorIntent.self ,provider: ColoredTimeline() ){
-            entry in ColoredWidgetView(entry:entry)
+        AppIntentConfiguration(kind: "AccountWidget", intent: SelectAccountIntent.self, provider: AccountTimeline()) {
+            entry in AccountWidgetView(entry: entry)
         }
         .configurationDisplayName("Modify Widget")
         .description("Choose Account")
@@ -185,5 +137,5 @@ struct BankWidget: Widget {
 #Preview(as: .systemSmall) {
     BankWidget()
 } timeline: {
-    ColoredEntry(date: .now, widgetColor: WidgetColor.allAccounts.first!)
+    AccountEntry(date: .now, widgetAccount: WidgetAccount.allAccounts.first!)
 }
